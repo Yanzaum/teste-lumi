7b707e162ed4e53e55ef6b219768299ae892ce0d
Yanzaum <yandavid80@gmail.com>
2024-05-21 20:59:26 -0300

    feat: Add ExpressAdapter for HTTP server implementation

---
+++ backend/src/infra/http/HttpServer.ts
@@ -0,0 +1,37 @@
+import express, { Request, Response, Express } from "express";
+import cors from "cors";
+import HttpServer from "application/http/HttpServer";
+
+type HttpMethod = 'get' | 'post' | 'put' | 'delete' | 'patch';
+
+export class ExpressAdapter implements HttpServer {
+	app: Express;
+
+	constructor() {
+		this.app = express();
+		this.app.use(express.json());
+		this.app.use(cors())
+	}
+
+	register(method: HttpMethod, url: string, callback: Function): void {
+		this.app[method](url.replace(/\{|\}/g, ""), async (req: Request, res: Response) => {
+			try {
+				const output = await callback(req.params, req.body);
+				if (output.statusCode) {
+					res.status(output.statusCode);
+				}
+				res.json(output.body);
+			} catch (error: any) {
+				res.status(error.statusCode || 500).json({
+					message: error.message || "Internal Server Error",
+				});
+			}
+		});
+	}
+
+	listen(port: number): void {
+		this.app.listen(port, () => {
+			console.log(`Server is running on port ${port}`);
+		});
+	}
+}
