7f6e189a0bf16f08ce425a1c1133c5cbb886b904
Yanzaum <yandavid80@gmail.com>
2024-05-21 23:02:21 -0300

    feat: Add InvoiceController with API endpoints for retrieving invoices

---
+++ backend/src/infra/controller/InvoiceController.ts
@@ -0,0 +1,34 @@
+import HttpServer from "application/http/HttpServer";
+import ExtractInvoices from "application/useCase/ExtractInvoices";
+import GetAllInvoices from "application/useCase/GetAllInvoices";
+import GetInvoicesByCustomerNumber from "application/useCase/GetInvoicesByCustomerNumber";
+
+export default class InvoiceController {
+	constructor (
+		readonly httpServer: HttpServer,
+		readonly getAllInvoices: GetAllInvoices,
+        readonly getInvoicesByCustomerNumber: GetInvoicesByCustomerNumber,
+        readonly extractInvoices: ExtractInvoices
+	) {
+		httpServer.register("get", "/api/invoices", async function (params: any, body: any) {
+			const output = await getAllInvoices.execute();
+			return {
+				body: output
+			};
+		});
+
+        httpServer.register("get", "/api/invoices/:customerNumber", async function (params: any, body: any) {
+            const output = await getInvoicesByCustomerNumber.execute(params.customerNumber);
+            return {
+                body: output
+            };
+        });
+
+        httpServer.register("post", "/api/invoices/extract", async function (params: any, body: any) {
+            const output = await extractInvoices.execute();
+            return {
+                body: output
+            };
+        });
+	}
+}
