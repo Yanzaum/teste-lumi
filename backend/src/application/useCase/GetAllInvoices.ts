d3be06684b5f3cb58a808f6b169d86cd26a12b11
Yanzaum <yandavid80@gmail.com>
2024-05-23 19:43:55 -0300

    feat: Update GetAllInvoices to return formatted invoice data

--- backend/src/application/useCase/GetAllInvoices.ts
+++ backend/src/application/useCase/GetAllInvoices.ts
@@ -5,7 +5,31 @@ export default class GetAllInvoices {
 
     async execute() {
         const invoices = await this.invoiceRepository.getAll();
-
-        return invoices;
+        return invoices.map(invoice => ({
+            id: invoice.getId(),
+            customerNumber: invoice.getCustomerNumber().toString(),
+            reference: invoice.getReference(),
+            eletricPower: {
+                quantityKWh: invoice.getEletricPower()?.getQuantityKWh(),
+                value: invoice.getEletricPower()?.getPrice()
+            },
+            eletricSCEEE: {
+                quantityKWh: invoice.getEletricSCEEE()?.getQuantityKWh(),
+                value: invoice.getEletricSCEEE()?.getPrice()
+            },
+            eletricGDI: {
+                quantityKWh: invoice.getEletricGDI()?.getQuantityKWh(),
+                value: invoice.getEletricGDI()?.getPrice()
+            },
+            eletricCompensated: {
+                quantityKWh: invoice.getEletricCompensated()?.getQuantityKWh(),
+                value: invoice.getEletricCompensated()?.getPrice()
+            },
+            eletricHFP: {
+                quantityKWh: invoice.getEletricHFP()?.getQuantityKWh(),
+                value: invoice.getEletricHFP()?.getPrice()
+            },
+            publicLightingContribution: invoice.getPublicLightingContribution()
+        }));
     }
 }
