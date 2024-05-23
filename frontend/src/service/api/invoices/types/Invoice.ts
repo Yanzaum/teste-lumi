1a2d8b5141f301cc971b3ebbc1e93e0461765ffd
Yanzaum <yandavid80@gmail.com>
2024-05-23 19:44:18 -0300

    feat: update Invoice type to use nested objects for eletricPower, eletricSCEEE, eletricGDI, eletricCompensated, and eletricHFP fields

--- frontend/src/service/api/invoices/types/Invoice.ts
+++ frontend/src/service/api/invoices/types/Invoice.ts
@@ -2,17 +2,25 @@ export type Invoice = {
     id: string
     customerNumber: bigint
     reference: string
-    eletricPowerkWh?: number
-    eletricPowerValue?: number
-    eletricSCEEEkWh?: number
-    eletricSCEEEValue?: number
-    eletricGDIkWh?: number
-    eletricGDIValue?: number
-    eletricCompensatedkWh?: number
-    eletricCompensatedValue?: number
-    eletricHFPkWh?: number
-    eletricHFPValue?: number
-    publicLightingContributionValue?: number
-    createdAt: Date
-    updatedAt: Date
+    eletricPower?: {
+        kWh?: number
+        value?: number
+    }
+    eletricSCEEE?: {
+        kWh?: number
+        value?: number
+    }
+    eletricGDI?: {
+        kWh?: number
+        value?: number
+    }
+    eletricCompensated?: {
+        kWh?: number
+        value?: number
+    }
+    eletricHFP?: {
+        kWh?: number
+        value?: number
+    }
+    publicLightingContributionValue: number
 }
