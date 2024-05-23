645321a29e1f64d5521217726373d4ddaee252dc
Yanzaum <yandavid80@gmail.com>
2024-05-23 20:58:51 -0300

    refactor: update GetAllInvoices to filter invoices by customer number in execute() method

--- backend/src/application/useCase/GetAllInvoices.ts
+++ backend/src/application/useCase/GetAllInvoices.ts
@@ -3,8 +3,13 @@ import InvoiceRepository from "application/repository/InvoiceRepository";
 export default class GetAllInvoices {
     constructor(private readonly invoiceRepository: InvoiceRepository) {}
 
-    async execute() {
-        const invoices = await this.invoiceRepository.getAll();
+    async execute(search?: string) {
+        let invoices = await this.invoiceRepository.getAll();
+
+        if (search) {
+            invoices = invoices.filter(invoice => invoice.getCustomerNumber().toString().includes(search));
+        }
+
         return invoices.map(invoice => ({
             id: invoice.getId(),
             customerNumber: invoice.getCustomerNumber().toString(),
