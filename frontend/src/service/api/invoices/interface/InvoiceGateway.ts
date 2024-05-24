97eb3e451cf323528859c06f18d0e81edadb4dbc
Yanzaum <yandavid80@gmail.com>
2024-05-23 23:12:13 -0300

    feat: add downloadInvoice method to InvoiceGateway

--- frontend/src/service/api/invoices/interface/InvoiceGateway.ts
+++ frontend/src/service/api/invoices/interface/InvoiceGateway.ts
@@ -3,4 +3,5 @@ import { Invoice } from "../types/Invoice";
 export interface InvoiceGateway {
     getInvoices(search?: string): Promise<Invoice[]>;
     getInvoice(id: string): Promise<Invoice>;
+    downloadInvoice(id: string): Promise<Blob>;
 }
