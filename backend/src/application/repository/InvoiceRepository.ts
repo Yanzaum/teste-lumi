e7d0f163ae684a0aa1737662ce3214f92ce345da
Yanzaum <yandavid80@gmail.com>
2024-05-23 22:20:52 -0300

    refactor: add getById method to InvoiceRepository

--- backend/src/application/repository/InvoiceRepository.ts
+++ backend/src/application/repository/InvoiceRepository.ts
@@ -3,5 +3,6 @@ import Invoice from "domain/entity/Invoice";
 export default interface InvoiceRepository {
     save(invoice: Invoice): Promise<void>;
     getAll(): Promise<Invoice[]>;
+    getById(id: string): Promise<Invoice>;
     getByCustomerNumber(customerNumber: bigint): Promise<Invoice[]>;
 }
