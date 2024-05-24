2bb41ba7a998467c24d5a9284e8175ae7e922247
Yanzaum <yandavid80@gmail.com>
2024-05-23 23:11:41 -0300

    refactor: Update getColorsTremor to use getRandomColorsTremor

--- frontend/src/utils/getColorsTremor.ts
+++ frontend/src/utils/getColorsTremor.ts
@@ -8,16 +8,21 @@ function shuffleArray(array: Color[]): Color[] {
     return array;
 }
 
+const colors: Color[] = [
+    "green", "cyan", "slate", "sky", "gray", "zinc",
+    "neutral", "stone", "red", "orange", "amber",
+    "yellow", "lime", "emerald", "teal", "blue",
+    "indigo", "violet", "purple", "fuchsia", "pink", "rose"
+];
 
-export default function getColorsTremor(quantity: number): Color[] {
-    const colors: Color[] = [
-        "slate", "gray", "zinc", "neutral", "stone", "red", "orange", "amber",
-        "yellow", "lime", "green", "emerald", "teal", "cyan", "sky", "blue",
-        "indigo", "violet", "purple", "fuchsia", "pink", "rose"
-    ];
-
+export function getRandomColorsTremor(quantity: number): Color[] {
     const shuffledColors = shuffleArray([...colors]);
     
     const limitedQuantity = Math.min(quantity, colors.length);
     return shuffledColors.slice(0, limitedQuantity);
+}
+
+export function getColorsTremor(quantity: number): Color[] {
+    const limitedQuantity = Math.min(quantity, colors.length);
+    return colors.slice(0, limitedQuantity);
 }
